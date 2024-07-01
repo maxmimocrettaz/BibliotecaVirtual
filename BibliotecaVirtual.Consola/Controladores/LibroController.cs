4b8658fbc206905a8fd7246a83ed33a04e57762b
Marq240 <gastigar2024@gmail.com>
2024-06-30 22:30:19 -0300

    Adicion de funciones basicas de Cargar y Mostrar libros

---
+++ BibliotecaVirtual.Consola/Controladores/LibroController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BibliotecaVirtual.Consola.Modelos;
+
+
+namespace BibliotecaVirtual.Consola.Controladores
+{
+    public class LibroController
+    {
+        Repositorio repo = Repositorio.ObtenerInstancia();
+        public void GuardarLibro(Libro l)
+        {
+
+            
+
+            repo.libros.Add(l);
+
+
+
+
+
+        }
+
+        public  List<Libro> ObtenerLibros()
+        {
+
+            return repo.libros;
+        
+        
+        }
+        
+
+
+
+
+       
+
+
+    }
+}
---
+++ BibliotecaVirtual.Consola/Vistas/LibroVista.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BibliotecaVirtual.Consola.Controladores;
+using BibliotecaVirtual.Consola.Modelos;
+
+namespace BibliotecaVirtual.Consola.Vistas
+{
+    public class LibroVista
+    {
+        LibroController controladorLibros = new LibroController();
+
+
+        public void CargarDatosLibro()
+        {
+            Libro libroNuevo = new Libro();
+
+            Console.WriteLine("Autor:");
+
+            libroNuevo.Autor = Console.ReadLine();
+
+            Console.WriteLine("Titulo:");
+
+            libroNuevo.Titulo = Console.ReadLine();
+
+            Console.WriteLine("Categoría:");
+
+            libroNuevo.Categoria = Console.ReadLine();
+
+            Console.WriteLine("Año:");
+
+            libroNuevo.Año = int.Parse(Console.ReadLine());
+
+          controladorLibros.GuardarLibro(libroNuevo);
+            
+
+
+        }
+        
+        public void MostrarLibros()
+        {
+            List<Libro> listadoLibros = controladorLibros.ObtenerLibros();
+
+            Console.WriteLine("Listado de Libros cargados");
+
+            foreach (var item in listadoLibros)
+            {
+                Console.WriteLine($">Autor: {item.Autor} -Titulo: {item.Titulo} -Categoria: {item.Categoria} -Año: {item.Año} ");
+            }
+        }
+
+
+
+
+
+
+    }
+
+
+
+}
+

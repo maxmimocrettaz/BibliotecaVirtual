421716dd83d1390a5dc98567c8a5815611361772
FacundoTortul <143459289+FacundoTortul@users.noreply.github.com>
2024-07-05 20:13:39 -0300

    Agregada la vista de cada menu(x3) Algunas funciones

--- BibliotecaVirtual.Consola/Vistas/UsuarioVista.cs
+++ BibliotecaVirtual.Consola/Vistas/UsuarioVista.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Controladores
-using System.Modelos
+﻿using BibliotecaVirtual.Consola.Controladores;
+using BibliotecaVirtual.Consola.Modelos;
 
 namespace BibliotecaVirtual.Consola.Vistas
 {

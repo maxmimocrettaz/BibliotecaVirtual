d78d669301c7a14f4bef41571e4617167a083fa3
Ollocco <augustojoaquinsacco@gmail.com>
2024-07-05 18:01:23 -0300

    Creación de repositorio generico

---
+++ BibliotecaVirtual.Consola/Respositorios/Generico.cs
@@ -0,0 +1,65 @@
+using BibliotecaPrestamos.Model;
+
+namespace BibliotecaPrestamos.Repositorio
+{
+    internal class RepoGenerico<T> where T : BaseEntidad   
+    {
+        public T Crear(T obj)
+        {
+            using (AppDbContext context = new AppDbContext())
+            {
+                context.Database.EnsureCreated();
+                context.Set<T>().Add(obj);
+                context.SaveChanges();
+            }
+            return obj;
+        }
+
+        public T Actualizar(T obj)
+        {
+
+            using (AppDbContext context = new AppDbContext())
+            {
+                context.Set<T>().Update(obj);
+
+                context.SaveChanges();
+            }
+
+            return obj;
+
+        }
+
+        public T Eliminar(T obj)
+        {
+            using (AppDbContext context = new AppDbContext())
+            {
+                context.Database.EnsureCreated();
+                context.Set<T>().Remove(obj);
+                context.SaveChanges();
+            }
+            return obj;
+        }
+
+        public List<T> VerTodos()
+        {
+            List<T> libros;
+
+            using (AppDbContext context = new AppDbContext())
+            {
+                libros = context.Set<T>().ToList();
+            }
+            return libros;
+        }
+
+        public T Obtener(int id)
+        {
+            T obj;
+
+            using (AppDbContext context = new AppDbContext())
+            {
+                obj = context.Set<T>().Where(x => x.Id == id).First();
+            }
+            return obj;
+        }
+    }
+}

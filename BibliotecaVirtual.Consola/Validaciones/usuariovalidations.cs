e25a41feb0b756889d27f419145ce7b0b7c50ae2
maxicrettaz <maxicrettaz31@gmail.com>
2024-07-13 17:25:29 -0300

    validaciones para libro

--- BibliotecaVirtual.Consola/Validaciones/usuariovalidations.cs
+++ BibliotecaVirtual.Consola/Validaciones/usuariovalidations.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace BibliotecaVirtual.Consola.Validaciones
 {
-    public class usuariovalidations
+    public class usuariovalidations : AbstractValidator<usuariovalidations>
     {
+      public usuariovalidations()
+        {
+            RuleFor(x =>x)
+        }
     }
 }

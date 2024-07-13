66e80549ced7e1688422b8a96f48bff2574d3285
maxicrettaz <maxicrettaz31@gmail.com>
2024-07-13 17:33:20 -0300

    Repositorios completos

---
+++ BibliotecaVirtual.Consola/Respositorios/RepositorioPRestamo.cs
@@ -0,0 +1,29 @@
+﻿using BibliotecaVirtual.Consola.Modelos;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaVirtual.Consola.Respositorios
+{
+    public class RepositorioPRestamo
+    {
+
+        public List<Prestamo> VerTodos()
+        {
+            List<Prestamo> listaPrestamos;
+
+            using (AppDbContext context = new AppDbContext())
+            {
+                listaPrestamos = context.Set<Prestamo>()
+                    .Include(x => x.Libro)
+                    .Include(x => x.Usuario)
+                    .ToList();
+            }
+            return listaPrestamos;
+        }
+
+    }
+}
